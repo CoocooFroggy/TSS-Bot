810ac444185828f16fcd700c6883847086becb13
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2022-04-27 13:00:09 +0000

    Docker

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+FROM gradle:7.4.2-jdk17 AS TEMP_BUILD_IMAGE
+ENV APP_HOME=/usr/app/
+WORKDIR $APP_HOME
+COPY build.gradle settings.gradle $APP_HOME
+  
+COPY gradle $APP_HOME/gradle
+COPY --chown=gradle:gradle . /home/gradle/src
+USER root
+RUN chown -R gradle /home/gradle/src
+
+COPY . .
+RUN gradle shadowJar
+
+FROM eclipse-temurin:17
+ENV ARTIFACT_NAME='TSS Portable-1.0-all.jar'
+ENV APP_HOME=/usr/app/
+    
+WORKDIR $APP_HOME
+COPY --from=TEMP_BUILD_IMAGE $APP_HOME/build/libs/$ARTIFACT_NAME .
+RUN apt-get update
+RUN apt-get install -y \
+    autoconf \
+    autoconf-archive \
+    autogen \
+    automake \
+    libtool \
+    m4 \
+    make \
+    pkg-config \
+    libzip-dev \
+    build-essential \
+    checkinstall \
+    git \
+    libtool-bin \
+    libreadline-dev \
+    libusb-1.0-0-dev \
+    libplist-dev \
+    libcurl4-openssl-dev \
+    python-dev \
+    openssl
+COPY script.sh .
+RUN ./script.sh
+ENTRYPOINT exec java -jar "${ARTIFACT_NAME}"
--- script.sh
+++ script.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-cp /etc/apt/sources.list sources.list
-sed -Ei 's/^# deb-src /deb-src /' sources.list
-
-declare -a pkgs=(
-    "autoconf"
-    "autoconf-archive"
-    "autogen"
-    "automake"
-    "libtool"
-    "m4"
-    "make"
-    "pkg-config"
-    "libzip-dev"
-    "build-essential"
-    "checkinstall"
-    "git"
-    "libtool-bin"
-    "libreadline-dev"
-    "libusb-1.0-0-dev"
-    "libplist-dev"
-)
-
-for i in "${pkgs[@]}"
-do
-    apt-get -o Dir::Etc::SourceList=./sources.list source $i
-    ./configure --prefix=$HOME/installed
-    make
-    make install
-done
-
 # apt install -y \
 # autoconf \
 # autoconf-archive \
@@ -48,61 +18,57 @@ done
 # libusb-1.0-0-dev \
 # libplist-dev
 
-mkdir ~/installed/
-
-export PKG_CONFIG_PATH=$HOME/installed/lib/pkgconfig
-
 # libgeneral
 git clone 'https://github.com/tihmstar/libgeneral.git'
 cd libgeneral/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh
 make
 make install
 cd ../
 # libfragmentzip
 git clone 'https://github.com/tihmstar/libfragmentzip.git'
 cd libfragmentzip/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
 # libplist
 git clone 'https://github.com/libimobiledevice/libplist.git'
 cd libplist/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
 # libimobiledevice-glue
 git clone 'https://github.com/libimobiledevice/libimobiledevice-glue.git'
 cd libimobiledevice-glue/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
 # libirecovery
 git clone 'https://github.com/libimobiledevice/libirecovery.git'
 cd libirecovery/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
 # tsschecker
 git clone --recursive 'https://github.com/1Conan/tsschecker.git'
 cd tsschecker/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
 # liblzfse
 git clone 'https://github.com/lzfse/lzfse.git'
 cd lzfse/
-make install INSTALL_PREFIX=$HOME/installed/
+make install
 cd ../
 # img4tool
 git clone 'https://github.com/tihmstar/img4tool.git'
 cd img4tool/
-./autogen.sh --prefix=$HOME/installed/ CPPFLAGS=-I$HOME/installed/include/
+./autogen.sh 
 make
 make install
 cd ../
